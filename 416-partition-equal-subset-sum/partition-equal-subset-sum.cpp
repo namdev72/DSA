b44ee43572061f8d8ba150213688dd32d3fdac6c
Kartik Namdev <namdevkartik72@gmail.com>
2026-08-26 22:53:49 +0530

    Time: 171 ms (23.25%) | Memory: 17.4 MB (61.39%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,43 +1,62 @@
 class Solution {
 public:
+
+    bool fun(int n, vector<int>& nums, int sum)
+    {
+        // dp[i][j] =
+        // Can we make sum j using first i elements?
+
+        vector<vector<bool>> dp(
+            n + 1,
+            vector<bool>(sum + 1, false)
+        );
+
+        // Sum 0 can always be made
+        // by taking nothing
+        for(int i = 0; i <= n; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= sum; j++)
+            {
+                // Not take
+                bool notake = dp[i - 1][j];
+
+                // Take
+                bool take = false;
+
+                if(nums[i - 1] <= j)
+                {
+                    take = dp[i - 1][j - nums[i - 1]];
+                }
+
+                dp[i][j] = notake || take;
+            }
+        }
+
+        return dp[n][sum];
+    }
+
     bool canPartition(vector<int>& nums)
     {
         int n = nums.size();
 
         int total = 0;
-        for(int x : nums)
-            total += x;
 
+        for(int x : nums)
+        {
+            total += x;
+        }
+
+        // Odd sum cannot be divided equally
         if(total % 2 != 0)
             return false;
 
-        int target = total / 2;
+        int sum = total / 2;
 
-        vector<vector<bool>> dp(n,
-                                vector<bool>(target + 1, false));
-
-        // Base Case 1
-        for(int i = 0; i < n; i++)
-            dp[i][0] = true;
-
-        // Base Case 2
-        if(nums[0] <= target)
-            dp[0][nums[0]] = true;
-
-        for(int index = 1; index < n; index++)
-        {
-            for(int t = 1; t <= target; t++)
-            {
-                bool notTake = dp[index - 1][t];
-
-                bool take = false;
-                if(nums[index] <= t)
-                    take = dp[index - 1][t - nums[index]];
-
-                dp[index][t] = take || notTake;
-            }
-        }
-
-        return dp[n - 1][target];
+        return fun(n, nums, sum);
     }
 };
